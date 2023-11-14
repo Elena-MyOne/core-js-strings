803c441c92ed54e2f825e603cf1cc373af487a40
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-11-13 22:28:44 -0500

    feat: add solution for countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -309,8 +309,10 @@ function containsSubstring(str, sub) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const reg = /[aeiouyAEIOUY]/g;
+  const matches = str.match(reg);
+  return matches ? matches.length : 0;
 }
 
 /**
