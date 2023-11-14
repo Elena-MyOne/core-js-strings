8d11c2f2fd461d0902db4841f578a309cb744b5f
Elena-MyOne <yakovenko.ea72@gmail.com>
2023-11-13 22:55:05 -0500

    feat: add solution for unbracketTag

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -437,8 +437,9 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  const str2 = str.replace('<', '');
+  return str2.replace('>', '');
 }
 
 /**
